40eed3c408113ac6f3d202d7efabf5ed88962995
MargoMarm <margogerasimenko@gmail.com>
2023-06-09 22:28:29 +0200

    redo components with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
+import ContactForm from './ContactForm/ContactForm';
+import Contacts from './Contacts/Contacts';
+import Filter from './Filter/Filter';
+import { WrapperContent } from './App.styled';
+import { nanoid } from 'nanoid';
+import Notiflix from 'notiflix';
+import { useState, useEffect } from 'react';
+import initialContacts from './contacts.json';
+
 export const App = () => {
+	const [contacts, setContacts] = useState(
+		() => (JSON.parse(localStorage.getItem('contacts')) ?? initialContacts)
+	);
+	console.log('contatts first',contacts)
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+	
+	
+
+  const createContact = data => {
+    const { name, number } = data;
+    const contact = {
+      name,
+      number,
+      id: nanoid(),
+    };
+
+    if (
+      contacts.find(existingContact => existingContact.name === contact.name)
+    ) {
+      Notiflix.Notify.failure(`${contact.name} is already in your contacts`);
+    } else {
+      setContacts(prevContacts => [contact, ...prevContacts]);
+      Notiflix.Notify.success(
+        `${contact.name} has been successfully added to  your phonebook`
+      );
+    }
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const getFiltredContacts = () => {
+    const filtredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return filtredContacts;
+  };
+
+  const hadleFilterChange = ({ target }) => {
+    setFilter(target.value);
+  };
+	console.log(contacts.length)
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <WrapperContent>
+      <ContactForm createContact={createContact} />
+      {( contacts.length > 0) && (
+        <Contacts
+          deleteContact={deleteContact}
+          contacts={getFiltredContacts()}
+        ></Contacts>
+      )}
+
+      <Filter value={filter} onChange={hadleFilterChange}></Filter>
+    </WrapperContent>
   );
 };
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import { Input, AddButton, Form, Title } from './ContactForm.styled';
+
+const ContactForm = ({ createContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    switch (name) {
+      case 'name': {
+        setName(value.trim());
+        break;
+      }
+      case 'number': {
+        setNumber(value.trim());
+        break;
+      }
+
+      default: {
+        console.log('There is no such input like that');
+      }
+    }
+  };
+  const addContact = e => {
+    e.preventDefault();
+    createContact({name, number} );
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <>
+      <Title>Phonebook</Title>
+      <Form onSubmit={addContact}>
+        <Input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          placeholder="Name"
+          onChange={handleChange}
+          value={name}
+        />
+        <Input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          placeholder="Number"
+          onChange={handleChange}
+          value={number}
+        />
+        <AddButton type="submit">Add contacts</AddButton>
+      </Form>
+    </>
+  );
+};
+
+
+export default ContactForm;
